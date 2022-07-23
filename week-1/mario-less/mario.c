e1386348680cd2b51dceddcd3dc1614b491cb9ec
RiczCalixto <rcalixtos@gmail.com>
2022-07-23 15:46:07 -0300

    feat: complete mario - less assignment

---
+++ week-1/mario-less/mario.c
@@ -0,0 +1,28 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void print_n_times(int times, char character);
+
+int main(void)
+{
+  int height;
+  do
+  {
+    height = get_int("Height (beteern 1 and 8): ");
+  } while (height < 1 || height > 8);
+
+  for (int row = 1; row <= height; row++)
+  {
+    print_n_times(height - row, '.');
+    print_n_times(row, '#');
+    printf("\n");
+  }
+}
+
+void print_n_times(int n_times, char character)
+{
+  for (int start = 0; start < n_times; start++)
+  {
+    printf("%c", character);
+  }
+}
